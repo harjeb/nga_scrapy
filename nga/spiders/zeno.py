2470a5b33e6e2ad3949261aad57b64db7d7c75cb
name <chenjun.ju@pactera.com>
2018-03-02 17:59:21 +0800

    update

--- nga/spiders/zeno.py
+++ nga/spiders/zeno.py
@@ -4,15 +4,18 @@ import json
 import scrapy
 from nga.items import NgaItem
 from nga.utils import Util
-import datetime
+import time
+
+last24htime = 0
 
 class NgaSpider(scrapy.Spider):
     name = "nga"
     allowed_domains = ["bbs.ngacn.cc"]
+    postdate = None
 
 
     def start_requests(self):
-        url = "http://bbs.ngacn.cc/thread.php?&lite=js&page=10"
+        url = "http://bbs.ngacn.cc/thread.php?&lite=js"
         cookies = {
             'ngaPassportCid':"Z8is8l0tipk613c99ockr5a1iqvgm718a9410q0j",
             'ngaPassportUid':"14156059",
@@ -23,14 +26,33 @@ class NgaSpider(scrapy.Spider):
     def parse(self, response):
         item = NgaItem()
         util = Util()
+        endpage = 0
+        jdict = util.from_js(response.body)
+        timenow = jdict['time']
+        last24htime = int(timenow) - 88200  #NGA帖子需要在发帖30分钟后才统计板块,所以这里会计算从24h30mins前到现在的帖子情况
+        jdata = jdict['data']['__T']['0']
+        postdate = jdata['postdate']
+        while int(postdate) > last24htime:
+            url = 'http://bbs.ngacn.cc/thread.php?&lite=js' + '&page=%s' % endpage
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+            endpage += 1
+
+
+    def parse_dir_contents(self, response):
+        item = NgaItem()
+        util = Util()
         jdict = util.from_js(response.body)
         for i in xrange(35):
-            jdata = jdict['data']['__T'][str(i)]
-            item['posttitle'] = jdata['subject']
-            item['postnum'] = jdata['replies']
-            if len(jdata['parent']['2'])>0:
-                item['postarea'] = jdata['parent']['2']
-            yield item
+            if str(i) in jdict['data']['__T']:
+                jdata = jdict['data']['__T'][str(i)]
+                postdate = jdata['postdate']
+                if int(postdate) > last24htime:
+                    item['posttitle'] = jdata['subject']
+                    item['postnum'] = jdata['replies']
+                    if 'parent' in jdata:
+                        item['postarea'] = jdata['parent']['2']
+                        yield item
+
 
 
 
