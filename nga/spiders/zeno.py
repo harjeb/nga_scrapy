95763493a15df2ca88855026cc7d739fffb38036
name <chenjun.ju@pactera.com>
2018-03-05 16:47:43 +0800

    update

--- nga/spiders/zeno.py
+++ nga/spiders/zeno.py
@@ -24,15 +24,7 @@ class NgaSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        item = NgaItem()
-        util = Util()
-        endpage = 0
-        jdict = util.from_js(response.body)
-        timenow = jdict['time']
-        last24htime = int(timenow) - 88200  #NGA帖子需要在发帖30分钟后才统计板块,所以这里会计算从24h30mins前到现在的帖子情况
-        jdata = jdict['data']['__T']['0']
-        postdate = jdata['postdate']
-        while int(postdate) > last24htime:
+        for endpage in xrange(2000):
             url = 'http://bbs.ngacn.cc/thread.php?&lite=js' + '&page=%s' % endpage
             yield scrapy.Request(url, callback=self.parse_dir_contents)
             endpage += 1
@@ -49,6 +41,7 @@ class NgaSpider(scrapy.Spider):
                 if int(postdate) > last24htime:
                     item['posttitle'] = jdata['subject']
                     item['postnum'] = jdata['replies']
+                    item['posttime'] = jdata['postdate']
                     if 'parent' in jdata:
                         item['postarea'] = jdata['parent']['2']
                         yield item
